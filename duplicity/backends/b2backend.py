ce54dc7b507aa695513de221cf563ac1dfbaa83e
Adam Jacobs <alj@boxyfrog.com>
2021-05-21 22:42:55 -0700

    need to declare names global so other methods can see them

--- duplicity/backends/b2backend.py
+++ duplicity/backends/b2backend.py
@@ -65,6 +65,7 @@ class B2Backend(duplicity.backend.Backend):
         """
         duplicity.backend.Backend.__init__(self, parsed_url)
 
+        global DownloadDestLocalFile, FileVersionInfoFactory
         try: # prefer to use public API methods
             from b2sdk.v1.api import B2Api
             from b2sdk.v1.account_info import InMemoryAccountInfo
