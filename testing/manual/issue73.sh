0a439142a8e222b531543d008133d39bf498118e
yova <yova@freedomhost.de>
2021-09-22 11:23:51 +0000

    --volsize is irrelevant for first part of test https://gitlab.com/duplicity/duplicity/-/merge_requests/74#note_683851952

--- testing/manual/issue73.sh
+++ testing/manual/issue73.sh
@@ -5,7 +5,7 @@ dd if=/dev/zero of=/tmp/testfile bs=1000000 count=100
 echo Running duplicity without multipart
 duplicity \
   --full-if-older-than 30D --verbosity 9 \
-  --name home --no-encryption --volsize 20 \
+  --name home --no-encryption \
   /tmp/testfile swift://issue73
 
 duplicity \
