23e76c28c90e99294a6849ffb8eb8761bd744e07
Kenneth Loafman <kenneth@loafman.com>
2022-04-24 12:17:52 -0500

    chg:test: Add shims to path, create volume during build.

--- testing/docker/build.sh
+++ testing/docker/build.sh
@@ -26,4 +26,5 @@ cd `dirname $0`
 
 export DOCKER_BUILDKIT=0
 
-docker build --compress --tag firstprime/duplicity_test --file duplicity_test/Dockerfile .
+docker build --compress --tag firstprime/duplicity_test --fileq duplicity_test/Dockerfile .
+docker volume create toxworkdir
