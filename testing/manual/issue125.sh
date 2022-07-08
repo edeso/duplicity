043b2af79f6d52a8552ee7287ae30513d8bc0c35
Kenneth Loafman <kenneth@loafman.com>
2022-07-08 13:29:03 -0500

    chg:test: Lower test to ulimit 2048 and reverse filename order.

--- testing/manual/issue125.sh
+++ testing/manual/issue125.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -e -v
 
 cd `dirname $0`/../..
 
@@ -9,29 +9,31 @@ export PASSPHRASE=""
 export MYTESTKEY=2D7CDA824BB7660CDE350B228C2938CE3CAEB354
 
 # Clean up
-rm -rf /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/mytestbackup
+rm -rf /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
 
-ulimit -n 102400
+ulimit -n 2048
+ulimit -n
 
 mkdir /tmp/testfiles
 mkdir /tmp/testbackup
 
 # Full backup plus 253 incremental backups
-for i in {1..254}
+for i in {254..1}
 do
-    dd if=/dev/urandom of=/tmp/testfiles/test${i}.file bs=1M count=1
-    bin/duplicity --name mytestbackup --encrypt-key ${MYTESTKEY} /tmp/testfiles par2+file:///tmp/testbackup
+    echo "File test${i}"
+    dd if=/dev/urandom of=/tmp/testfiles/test${i}.file bs=1M count=1 status=none
+    bin/duplicity --name issue125 --encrypt-key ${MYTESTKEY} --no-print -v0 /tmp/testfiles par2+file:///tmp/testbackup
 done
 
 # verify works
-bin/duplicity verify --name mytestbackup --encrypt-key ${MYTESTKEY} par2+file:///tmp/testbackup /tmp/testfiles
+bin/duplicity verify --name issue125 --encrypt-key ${MYTESTKEY} par2+file:///tmp/testbackup /tmp/testfiles
 
 # 255th backup set
-dd if=/dev/urandom of=/tmp/testfiles/test255.file bs=1M count=1
-bin/duplicity --name mytestbackup --encrypt-key ${MYTESTKEY} /tmp/testfiles par2+file:///tmp/testbackup
+dd if=/dev/urandom of=/tmp/testfiles/test255.file bs=1M count=1 status=none
+bin/duplicity --name issue125 --encrypt-key ${MYTESTKEY} --no-print -v0 /tmp/testfiles par2+file:///tmp/testbackup
 
 # verify crashes with "filedescriptor out of range in select()"
-bin/duplicity verify -v9 --name mytestbackup --encrypt-key ${MYTESTKEY} par2+file:///tmp/testbackup /tmp/testfiles
+bin/duplicity verify --name issue125 --encrypt-key ${MYTESTKEY} par2+file:///tmp/testbackup /tmp/testfiles
 
 # Clean up
-rm -r /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/mytestbackup
+rm -r /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
