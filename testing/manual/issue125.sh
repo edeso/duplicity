10236bc5ab27e55af59c429c514cc04078f90af7
Kenneth Loafman <kenneth@loafman.com>
2022-08-16 11:37:45 -0500

    fix:usr: Remove unused GPG file handles.
    - removed status and logger filehandles for decrypt
    - testing/manual/issue125 now runs with 'ulimit -n 1536'

--- testing/manual/issue125.sh
+++ testing/manual/issue125.sh
@@ -8,29 +8,22 @@ export PYTHONPATH=`pwd`
 export PASSPHRASE=""
 export MYTESTKEY=2D7CDA824BB7660CDE350B228C2938CE3CAEB354
 
-# Clean up
-rm -rf /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
-
-ulimit -n 2048
+ulimit -n 1536
 ulimit -n
+
+# Clean up if previous failure
+rm -rf /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
 
 mkdir /tmp/testfiles
 mkdir /tmp/testbackup
 
-# Full backup plus 253 incremental backups
-for i in {254..1}
+# Full backup plus 256 incremental backups
+for i in {1..256}
 do
-    echo "File test${i}"
-    dd if=/dev/urandom of=/tmp/testfiles/test${i}.file bs=1M count=1 status=none
+    echo "File test${i}.file"
+    dd if=/dev/urandom of="/tmp/testfiles/test${i}.file" bs=1M count=1 status=none
     bin/duplicity --name issue125 --encrypt-key ${MYTESTKEY} --no-print -v0 /tmp/testfiles file:///tmp/testbackup
 done
-
-# verify works
-bin/duplicity verify --name issue125 --encrypt-key ${MYTESTKEY} file:///tmp/testbackup /tmp/testfiles
-
-# 255th backup set
-dd if=/dev/urandom of=/tmp/testfiles/test255.file bs=1M count=1 status=none
-bin/duplicity --name issue125 --encrypt-key ${MYTESTKEY} --no-print -v0 /tmp/testfiles file:///tmp/testbackup
 
 # verify crashes with "filedescriptor out of range in select()"
 bin/duplicity verify --name issue125 --encrypt-key ${MYTESTKEY} file:///tmp/testbackup /tmp/testfiles
