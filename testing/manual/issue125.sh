47a9064f8e9b0fe0301f5f18b7b53fe1333121c2
Kenneth Loafman <kenneth@loafman.com>
2022-08-17 13:26:47 -0500

    fix:usr: Remove status_fd if no sign_key in gpg.py.
    - updated issue125.sh to use testing/gnupg keys
    - issue125.sh passes with `ulimit 1024`

--- testing/manual/issue125.sh
+++ testing/manual/issue125.sh
@@ -5,15 +5,22 @@ set -e -v
 cd `dirname $0`/../..
 
 export PYTHONPATH=`pwd`
-export PASSPHRASE=""
-export MYTESTKEY=2D7CDA824BB7660CDE350B228C2938CE3CAEB354
 
-ulimit -n 1536
+# use testing/gnupg forever keys
+export GNUPGHOME="testing/gnupg"
+export PASSPHRASE="test"
+export SIGN_KEY="839E6A2856538CCF"
+export SIGN_PASSPHRASE="test"
+export ENCRYPT_KEY1="839E6A2856538CCF"
+export ENCRYPT_KEY2="453005CE9B736B2A"
+
+ulimit -n 1024
 ulimit -n
 
 # Clean up if previous failure
 rm -rf /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
 
+# Make test dirs
 mkdir /tmp/testfiles
 mkdir /tmp/testbackup
 
@@ -22,11 +29,11 @@ for i in {1..256}
 do
     echo "File test${i}.file"
     dd if=/dev/urandom of="/tmp/testfiles/test${i}.file" bs=1M count=1 status=none
-    bin/duplicity --name issue125 --encrypt-key ${MYTESTKEY} --no-print -v0 /tmp/testfiles file:///tmp/testbackup
+    bin/duplicity --name issue125 --encrypt-key ${ENCRYPT_KEY1} --no-print -v0 /tmp/testfiles file:///tmp/testbackup
 done
 
-# verify crashes with "filedescriptor out of range in select()"
-bin/duplicity verify --name issue125 --encrypt-key ${MYTESTKEY} file:///tmp/testbackup /tmp/testfiles
+# Verify may crash with "filedescriptor out of range in select()"
+bin/duplicity verify --name issue125 --encrypt-key ${ENCRYPT_KEY1} file:///tmp/testbackup /tmp/testfiles
 
 # Clean up
 rm -r /tmp/testfiles /tmp/testbackup ~/.cache/duplicity/issue125
