282007a4686512bd907623985bd446c9aaf4316e
Kenneth Loafman <kenneth@loafman.com>
2023-03-03 10:30:13 -0600

    fix:test:  Skip test_path::test_compare on non-native.
    Skip on GitLab and Launchpad build systems.
    Works fine on Linux and macOS.

--- testing/unit/test_path.py
+++ testing/unit/test_path.py
@@ -45,10 +45,10 @@ class PathTest(UnitTestCase):
         p.deltree()
         assert not p.type, p.type
 
-    @unittest.skipIf(os.path.exists(u"/.dockenv") or                    # Docker
-                     os.environ.get(u"USER", None) == u"buildd" or      # Launchpad
-                     os.environ.get(u"CI_JOB_STAGE", None) == u"test",  # GitLab
-                     u"Skip on Docker, Launchpad, or Gitlab")
+    @unittest.skipIf(os.path.exists(u"/.dockenv") or                        # Docker
+                     os.environ.get(u"USER", None) == u"buildd" or          # Launchpad
+                     os.environ.get(u"NON_NATIVE", None) == u"true",        # GitLab
+                     u"Skip on non-native environs")
     def test_compare(self):
         u"""Test directory comparisons"""
         assert not os.system(u"cp -pR {0}/testfiles/dir1/ {0}/testfiles/output".format(_runtest_dir))
