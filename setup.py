d67ef9054121a47c9168478e90a4e7b7071d39c8
Kenneth Loafman <kenneth@loafman.com>
2023-10-20 12:44:23 -0500

    chg:pkg: Update version for LP.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ if not (sys.version_info[0] == 3 and sys.version_info[1] >= 8):
     sys.exit(1)
 
 
-Version = "2.1.4"
+Version = "2.1.5.dev"
 scm_version_args = {
     "tag_regex": r"^(?P<prefix>rel.)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     "local_scheme": "no-local-version",
