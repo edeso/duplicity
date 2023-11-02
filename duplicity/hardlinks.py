68f857fed026841ac56563877dd764a844a4d125
Kenneth Loafman <kenneth@loafman.com>
2023-11-02 14:04:01 -0500

    new:usr: Fix size saved calc.

--- duplicity/hardlinks.py
+++ duplicity/hardlinks.py
@@ -6,30 +6,32 @@ from humanize import intcomma
 
 def get_hardlinks(path):
     saved = 0
+    total = 0
     inodes = dict()
     it = os.scandir(path)
     for entry in it:
         inode = entry.inode()
         nlinks = entry.stat().st_nlink
+        size = entry.stat().st_size
+        total += size
         if nlinks > 1 and not entry.is_symlink():
-            size = entry.stat().st_size
             if inode not in inodes:
                 inodes[inode] = []
-                inodes[inode].append(entry.path)
             else:
-                inodes[inode].append(entry.path)
                 saved += size
-            print(f"{entry.path}, {nlinks}, {size}")
-    return inodes, saved
+            inodes[inode].append(entry.path)
+            print(f"{entry.path}, {nlinks}, {intcomma(size)}")
+    return inodes, saved, total
 
 
 if __name__ == "__main__":
     try:
         path = sys.argv[1]
-    except:
+    except Exception:
         path = "/usr/bin"
 
     print(f"Scanning {path} for hardlinks")
     start = time.time()
-    inodes, saved = get_hardlinks(path)
-    print(f"Elapsed: {time.time()-start} for {len(inodes)} entries.\nWould save {intcomma(saved)} bytes on backup.")
+    inodes, saved, total = get_hardlinks(path)
+    print(f"Elapsed: {time.time()-start:.4f} for {len(inodes)} entries.")
+    print(f"Would save {intcomma(saved)} bytes of {intcomma(total)} without hard link support.")
