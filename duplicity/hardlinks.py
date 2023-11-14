a02dd5620e66bd2c93c200ed00138ca61ccf5b70
Kenneth Loafman <kenneth@loafman.com>
2023-11-14 13:10:23 -0600

    new:usr: More improvements.
    [ci skip]

--- duplicity/hardlinks.py
+++ duplicity/hardlinks.py
@@ -1,6 +1,28 @@
 import os
 import sys
 import time
+import platform
+
+exclude_lists = {
+    "Darwin": [
+        "/System/Volumes/Data/private",
+        "/Volumes",
+        "/private",
+        "/tmp",
+        "/var",
+    ],
+    "Linux": [
+        "/boot",
+        "/dev",
+        "/lost+found",
+        "/media",
+        "/mnt",
+        "/proc",
+        "/swapfile",
+        "/tmp",
+        "/var",
+    ],
+}
 
 
 class HardLinks:
@@ -10,26 +32,43 @@ class HardLinks:
         self.num_files = 0
         self.num_nlinks = 0
 
-    def get_hardlinks(self, path, follow_symlinkks=False):
+        self.exclude = exclude_lists[platform.system()]
+
+    def get_hardlinks(
+        self,
+        path,
+        follow_symlinkks=False,
+    ):
         our_dev = os.stat(path).st_dev
         for entry in os.scandir(path):
+            if entry.path in self.exclude:
+                continue
             self.num_entries += 1
+            try:
+                stat = entry.stat(follow_symlinks=follow_symlinkks)
+            except OSError as e:
+                print(str(e))
+                continue
+            if stat.st_dev != our_dev:
+                # stay on same filesystem
+                continue
             if entry.is_file(follow_symlinks=follow_symlinkks):
                 self.num_files += 1
                 inode = entry.inode()
-                stat = entry.stat(follow_symlinks=follow_symlinkks)
                 if stat.st_nlink > 1:
                     self.num_nlinks += 1
                     if inode not in self.inodes:
                         # first of this inode
-                        self.inodes[inode] = []
-                    # subsequent hard link
-                    self.inodes[inode].append(entry)
-            if entry.is_dir(follow_symlinks=follow_symlinkks) and entry.stat().st_dev == our_dev:
+                        self.inodes[inode] = [entry]
+                    else:
+                        # sub sequent hard link
+                        self.inodes[inode].append(entry)
+            if entry.is_dir(follow_symlinks=follow_symlinkks):
                 try:
                     self.get_hardlinks(entry.path)
-                except (OSError, PermissionError) as e:
+                except OSError as e:
                     print(str(e))
+                    continue
 
     def print_summary(self):
         hlinks_expanded = 0
@@ -58,14 +97,14 @@ if __name__ == "__main__":
     except Exception:
         path = "/usr"
 
-    follow = False
+    follow = True
 
     print(f"\nScanning {path} for hardlinks, follow_symlinks={follow}")
     start = time.time()
     hl = HardLinks()
     hl.get_hardlinks(path, follow_symlinkks=follow)
     print(
-        f"Elapsed: {time.time()-start:.4f} for {len(hl.inodes):,} "
-        f"inodes in {hl.num_nlinks:,} out of {hl.num_files:,} files."
+        f"Elapsed: {time.time()-start:.4f} seconds for {len(hl.inodes):,} "
+        f"inodes in {hl.num_nlinks:,} hardlinks out of {hl.num_files:,} files."
     )
     hl.print_summary()
