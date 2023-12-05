cee32ca337d108b8bacc55de25b34d1d9d13ec2b
Kenneth Loafman <kenneth@loafman.com>
2023-12-05 13:42:05 -0600

    new:usr: Refactor.  Ignore common errors.  Less print.
    [ci skip]

--- duplicity/hardlinks.py
+++ duplicity/hardlinks.py
@@ -38,6 +38,17 @@ class HardLinks:
 
         self.exclude = default_excludes.get(platform.system(), "Linux")
 
+        self.oserrors = {
+            1: 0,  # operation not permitted
+            13: 0,  # permission denied
+        }
+        self.not_our_filesystem = 0
+        self.in_exclusions = 0
+
+        self.hlinks_expanded = 0
+        self.hlinks_supported = 0
+        self.hlinks_incomplete = 0
+
     def get_hardlinks(self, path: str, follow_symlinkks: bool = False) -> None:
         our_dev = os.stat(path).st_dev
         for entry in os.scandir(path):
@@ -46,12 +57,16 @@ class HardLinks:
             try:
                 stat_res = entry.stat(follow_symlinks=follow_symlinkks)
             except OSError as e:
-                print(str(e))
+                if e.errno in self.oserrors:
+                    self.oserrors[e.errno] += 1
+                else:
+                    print(str(e))
                 continue
 
             if stat_res.st_dev != our_dev:
                 # stay on same filesystem
-                print(f"Skipping {entry.path}.  Not on our filesystem.")
+                # print(f"Skipping {entry.path}.  Not on our filesystem.")
+                self.not_our_filesystem += 1
                 continue
 
             if entry.is_file(follow_symlinks=follow_symlinkks):
@@ -71,7 +86,8 @@ class HardLinks:
                 for patt in self.exclude:
                     if entry.path.startswith(patt):
                         # some paths are trouble
-                        print(f"Skipping {entry.path}.  In exclusions.")
+                        # print(f"Skipping {entry.path}.  In exclusions.")
+                        self.in_exclusions += 1
                         skip = True
                         break
                 if skip:
@@ -81,7 +97,10 @@ class HardLinks:
                 try:
                     self.get_hardlinks(entry.path)
                 except OSError as e:
-                    print(str(e))
+                    if e.errno in self.oserrors:
+                        self.oserrors[e.errno] += 1
+                    else:
+                        print(str(e))
                     continue
 
     def dump_hardlinks(self, filename: str = "/tmp/hardlinks.json") -> None:
@@ -101,24 +120,30 @@ class HardLinks:
             fd.write(json.dumps(hardlinks, indent=2))
 
     def print_summary(self) -> None:
-        hlinks_expanded = 0
-        hlinks_supported = 0
-        hlinks_incomplete = 0
-
         print(f"\nSummary of {path}:")
 
         for inode in self.inodes:
             entry = self.inodes[inode][0]
-            hlinks_found = len(self.inodes[inode])
-            hlinks_expanded += entry.stat().st_size * hlinks_found
-            hlinks_supported += entry.stat().st_size
-            hlinks_outside = entry.stat().st_nlink - hlinks_found
-            if hlinks_outside:
-                hlinks_incomplete += entry.stat().st_nlink - hlinks_found
+            self.hlinks_found = len(self.inodes[inode])
+            self.hlinks_expanded += entry.stat().st_size * self.hlinks_found
+            self.hlinks_supported += entry.stat().st_size
+            self.hlinks_outside = entry.stat().st_nlink - self.hlinks_found
+            if self.hlinks_outside:
+                self.hlinks_incomplete += entry.stat().st_nlink - self.hlinks_found
                 print(
-                    f"Inode at {entry.path} has {hlinks_outside:,} hardlinks outside this directory. "
-                    f"({entry.stat().st_nlink:,} > {hlinks_found:,})"
+                    f"Inode at {entry.path} has {self.hlinks_outside:,} hardlinks outside this directory. "
+                    f"({entry.stat().st_nlink:,} > {self.hlinks_found:,})"
                 )
+
+        for errno in sorted(self.oserrors.keys()):
+            if self.oserrors[errno]:
+                print(f"Encountered {self.oserrors[errno]:,} errors: {os.strerror(errno)}")
+
+        if self.in_exclusions:
+            print(f"Found {self.in_exclusions:,} dirs in exclusions list.")
+
+        if self.not_our_filesystem:
+            print(f"Found {self.not_our_filesystem:,} dirs not in our filesystem.")
 
         print(
             f"Elapsed (secs):               {f'{time.time()-self.start:.4f}':>20}\n"
@@ -127,10 +152,10 @@ class HardLinks:
             f"reg files:                    {f'{self.num_files:,}':>20}\n"
             f"dir entries:                  {f'{self.num_entries:,}':>20}\n"
             f"directories:                  {f'{self.num_dirs:,}':>20}\n"
-            f"Size if hardlinks expanded:   {f'{hlinks_expanded:,}':>20}\n"
-            f"Size if hardlinks supported:  {f'{hlinks_supported:,}':>20}\n"
-            f"Hardlink support would save:  {f'{hlinks_expanded-hlinks_supported:,}':>20}\n"
-            f"inode paths outside dir:      {f'{hlinks_incomplete:,}':>20}"
+            f"Size if hardlinks expanded:   {f'{self.hlinks_expanded:,}':>20}\n"
+            f"Size if hardlinks supported:  {f'{self.hlinks_supported:,}':>20}\n"
+            f"Hardlink support would save:  {f'{self.hlinks_expanded-self.hlinks_supported:,}':>20}\n"
+            f"inode paths outside dir:      {f'{self.hlinks_incomplete:,}':>20}"
         )
 
 
