1b1076a03447fbd5c7951ac8a3b4fe2174f8a70e
Kenneth Loafman <kenneth@loafman.com>
2024-05-19 12:28:07 -0500

    chg:pkg: Bump version to 2.2.4.rc3.

--- duplicity/__init__.py
+++ duplicity/__init__.py
@@ -21,7 +21,7 @@
 
 import gettext
 
-__version__: str = "2.2.4rc2"
-__reldate__: str = "May 18, 2024"
+__version__: str = "2.2.4rc3"
+__reldate__: str = "May 19, 2024"
 
 gettext.install("duplicity", names=["ngettext"])
